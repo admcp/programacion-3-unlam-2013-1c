7a5995ce7ec722de31f51d9775aefff3a3c56b91
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-04-21 18:48:28 +0000

    Agregada la estructura mínima de los TDA Pila y Cola (codificadas interfaces y clases SIN implementar todavía), incluye 'metricas.xlsx'.

---
+++ practica2/trunk/TDA_Cola/src/ar/edu/unlam/programacion3/practica2/tda/Cola.java
@@ -0,0 +1,11 @@
+package ar.edu.unlam.programacion3.practica2.tda;
+
+public interface Cola<T> {
+	
+	public boolean empty();
+	public void offer(T elemento);
+	public T poll();
+	public T peek();
+	public void clear();
+	
+}

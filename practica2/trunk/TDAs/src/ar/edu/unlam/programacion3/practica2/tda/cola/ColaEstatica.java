c74abd18848ef854dd38ee9414c59fcb16eb2299
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-04-22 02:48:35 +0000

    Re-estructurada TODA la práctica 2: único proyecto para TODOS los TDA. Corregido problema de exportación/importación de Eclipse.

---
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/cola/ColaEstatica.java
@@ -0,0 +1,35 @@
+package ar.edu.unlam.programacion3.practica2.tda.cola;
+
+public class ColaEstatica<T> implements Cola<T> {
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void offer(T elemento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

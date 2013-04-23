ba69bf3a5df0886eca86ba6f4a89efb95de7bb76
brunosendras@gmail.com <brunosendras@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-04-23 02:25:40 +0000

    Se completó el código de la clase Punto2D y Punto3D.

--- practica3/trunk/Jerarquias/tests/ar/edu/unlam/programacion3/practica3/jerarquias/tests/punto/TestPunto3D.java
+++ practica3/trunk/Jerarquias/tests/ar/edu/unlam/programacion3/practica3/jerarquias/tests/punto/TestPunto3D.java
@@ -1,11 +1,11 @@
 package ar.edu.unlam.programacion3.practica3.jerarquias.tests.punto;
 
-import ar.edu.unlam.programacion3.practica3.jerarquias.punto.Punto3D;
+import ar.edu.unlam.programacion3.practica3.jerarquias.punto.*;
 
 public class TestPunto3D {
 
 	public static void main(String[] args) {
-
+	
 		Punto3D punto1 = new Punto3D(45, 5, 2); 
 		Punto3D punto2 = new Punto3D(45, 3, 2);
 		Punto3D punto3 = null;
@@ -21,6 +21,18 @@ public class TestPunto3D {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
+		// PRUEBA PROPUESTA EN LA GUIA
+		
+		Punto2D p2D = new Punto2D(0,0);
+		Punto3D p3D = new Punto3D(0,0,1);
+		Punto3D a3D = null;
+		System.out.println(p2D.equals(p3D));//false
+		System.out.println(p3D.equals(p2D));// false
+		System.out.println(p2D.equals(a3D));//false
+		System.out.println(p3D.equals(a3D));//false
+		a3D = new Punto3D(0,0,0);
+		System.out.println(p2D.equals(a3D));//false
+		System.out.println(a3D.equals(p2D));//false
 	}
 }

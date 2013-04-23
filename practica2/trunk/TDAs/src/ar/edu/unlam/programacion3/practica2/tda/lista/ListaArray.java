68eb523a3fc75a1ad22805fb442eb62d1384a454
anlucki <anlucki@gmail.com>
2013-04-23 20:44:53 +0000

    Codificada ListaEnlazada de acuerdo con la API de Java.

---
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/lista/ListaArray.java
@@ -0,0 +1,82 @@
+package ar.edu.unlam.programacion3.practica2.tda.lista;
+
+public class ListaArray<T> implements Lista<T> {
+
+	private int size;
+	private Object[] arrayElementos;
+	
+	// Contrato con <<Lista>>
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(T elemento) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Object elemento) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reverse() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T set(int index, T elemento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, T elemento) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object elemento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(Object elemento) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

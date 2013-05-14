7fd1ff5a971df048d8b83a77dacc906c69e904fc
anlucki <anlucki@gmail.com>
2013-05-14 06:54:10 +0000

    Iniciada la etapa final de SEL. Creadas las clases que van a ser necesarias para IO, inversión y resolución de SEL. Actualizado el diagrama UML.

---
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/MatrizInvertible.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.programacion3.practica2.tda.sel;
+
+public class MatrizInvertible extends MatrizCuadrada {
+
+	public MatrizInvertible(int dimension) {
+		super(dimension);
+	}
+
+}
---
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/util/SerializadorDeSEL.java
@@ -0,0 +1,9 @@
+package ar.edu.unlam.programacion3.practica2.tda.sel.util;
+
+public class SerializadorDeSEL {
+
+	public static void main(String[] args) {
+		
+	}
+
+}

834101d837d3896ab93fd6317c988f30aa53e4f6
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-14 03:35:31 +0000

    Realizado el merge a pedido de los cambios de Hernán sobre los de Bruno: producto entre vectores, setColumna y setFila, restar, sumar, y producto en VectorFila y VectorColumna, mains en dichas clases, constructores en dichas clases. Agregadas también MatrizCuadrada y MatrizIdentidad. Actualizadas las métricas por un pequeño error (incompleto tiempo y LoC real en iteración "Metodos Norma 1, 2, inf en vector").

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/VectorColumna.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/VectorColumna.java
@@ -1,16 +1,35 @@
 package ar.edu.unlam.programacion3.practica2.tda.sel;
 
-
 public class VectorColumna extends VectorMath {
 
 	public VectorColumna(int cantidadComponentes) {
 		validarRangoDimension(cantidadComponentes, 1, Integer.MAX_VALUE);
-			
+
 		this.cantidadFilas = cantidadComponentes;
 		this.cantidadColumnas = 1;
 		coeficientes = new double[cantidadFilas][cantidadColumnas];
 	}
-	
+
+	public VectorColumna(MatrizMath matriz) {
+		cantidadFilas = matriz.cantidadFilas;
+		cantidadColumnas = 1;
+		coeficientes = new double[cantidadFilas][cantidadColumnas];
+		for (int i = 0; i < matriz.cantidadFilas; i++) {
+			for (int j = 0; j < 1; j++) {
+				coeficientes[i][j] = matriz.coeficientes[i][j];
+			}
+		}
+	}
+
+	public VectorColumna(double[] vec) {
+		cantidadFilas = vec.length;
+		cantidadColumnas = 1;
+		coeficientes = new double[cantidadFilas][cantidadColumnas];
+		for (int i = 0; i < vec.length; i++) {
+			coeficientes[i][0] = vec[i];
+		}
+	}
+
 	@Override
 	public int getCantidadComponentes() {
 		return cantidadFilas;
@@ -18,44 +37,98 @@ public class VectorColumna extends VectorMath {
 
 	@Override
 	public double getValorEn(int indice) {
-		validarRangoDimension(indice, 0, cantidadFilas - 1);		
-		
+		validarRangoDimension(indice, 0, cantidadFilas - 1);
+
 		return coeficientes[indice][0];
 	}
-	
+
 	public void setValorEn(int indice, double valor) {
 		validarRangoDimension(indice, 0, cantidadFilas - 1);
-		
+
 		coeficientes[indice][0] = valor;
 	}
 
-	@Override
-   public double normaUno() {
-	   double norma = 0;
-		for(int i = 0; i < cantidadFilas; i++) {
-			norma += Math.abs(coeficientes[i][0]);
-	   }
-	   return norma;
-   }
+	// Suma, Resta, Multiplicacion
+	public static VectorColumna sumar(VectorColumna operando1, VectorColumna operando2) {
+		return new VectorColumna(MatrizMath.sumar(operando1, operando2));
+	}
 
-	@Override
-   public double normaDos() {
-	   double norma = 0;
-		for(int i = 0; i < cantidadFilas; i++) {
-			norma += Math.pow(coeficientes[i][0], 2);
-	   }
-	   return Math.sqrt(norma);
-   }
+	public static VectorColumna restar(VectorColumna operando1, VectorColumna operando2) {
+		return new VectorColumna(MatrizMath.restar(operando1, operando2));
+	}
 
+	public static VectorColumna productoEscalar(VectorColumna operando, double escalar) {
+		return new VectorColumna(MatrizMath.productoEscalar(operando, escalar));
+	}
+	
 	@Override
-   public double normaInfinito() {
+	public double normaUno() {
 		double norma = 0;
-		for(int i = 0; i < cantidadFilas; i++) {
+		for (int i = 0; i < cantidadFilas; i++) {
+			norma += Math.abs(coeficientes[i][0]);
+		}
+		return norma;
+	}
+
+	@Override
+	public double normaDos() {
+		double norma = 0;
+		for (int i = 0; i < cantidadFilas; i++) {
+			norma += Math.pow(coeficientes[i][0], 2);
+		}
+		return Math.sqrt(norma);
+	}
+
+	@Override
+	public double normaInfinito() {
+		double norma = 0;
+		for (int i = 0; i < cantidadFilas; i++) {
 			if (coeficientes[i][0] > norma) {
 				norma = Math.abs(coeficientes[0][i]);
 			}
-		}		
-	   return norma;
-   }
+		}
+		return norma;
+	}
+
+	// MAIN PARA EL TEST
+	public static void main(String[] args) {
+		VectorColumna vec1, vec2, aux;
+		vec1 = new VectorColumna(2);
+		vec2 = new VectorColumna(2);
+
+		vec1.setValorEn(0, 1);
+		vec1.setValorEn(1, 1);
+
+		vec2.setValorEn(0, 2);
+		vec2.setValorEn(1, 2);
+
+		for (int i = 0; i < vec1.getCantidadComponentes(); i++) {
+			System.out.println(vec1.getValorEn(i));
+		}
+
+		for (int i = 0; i < vec2.getCantidadComponentes(); i++) {
+			System.out.println(vec2.getValorEn(i));
+		}
+
+		System.out.println("Suma");
+		aux = new VectorColumna(sumar(vec1, vec2));
+
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+
+		System.out.println("Resta");
+		aux = restar(vec1, vec2);
+
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+
+		System.out.println("Producto escalar x7");
+		aux = productoEscalar(vec1, 7);
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+	}
 
 }
--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/VectorFila.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/VectorFila.java
@@ -1,16 +1,26 @@
 package ar.edu.unlam.programacion3.practica2.tda.sel;
 
-
 public class VectorFila extends VectorMath {
 
 	public VectorFila(int cantidadComponentes) {
 		validarRangoDimension(cantidadComponentes, 1, Integer.MAX_VALUE);
-		
+
 		this.cantidadFilas = 1;
 		this.cantidadColumnas = cantidadComponentes;
 		coeficientes = new double[cantidadFilas][cantidadColumnas];
 	}
-	
+
+	public VectorFila(MatrizMath matriz) {
+		cantidadFilas = 1;
+		cantidadColumnas = matriz.getCantidadColumnas();
+		coeficientes = new double[cantidadFilas][cantidadColumnas];
+		for (int i = 0; i < cantidadFilas; i++) {
+			for (int j = 0; j < cantidadColumnas; j++) {
+				coeficientes[i][j] = matriz.coeficientes[i][j];
+			}
+		}
+	}
+
 	@Override
 	public int getCantidadComponentes() {
 		return cantidadColumnas;
@@ -19,43 +29,100 @@ public class VectorFila extends VectorMath {
 	@Override
 	public double getValorEn(int indice) {
 		validarRangoDimension(indice, 0, cantidadColumnas - 1);
-		
+
 		return coeficientes[0][indice];
 	}
 
 	@Override
 	public void setValorEn(int indice, double valor) {
 		validarRangoDimension(indice, 0, cantidadColumnas - 1);
-		
-		coeficientes[0][indice] = valor;		
+
+		coeficientes[0][indice] = valor;
+	}
+
+	// Operaciones Sumar, Restar, Producto Escalar
+
+	public static VectorFila sumar(VectorFila operando1, VectorFila operando2) {
+		return new VectorFila(MatrizMath.sumar(operando1, operando2));
+	}
+
+	public static VectorFila restar(VectorFila operando1, VectorFila operando2) {
+		return new VectorFila(MatrizMath.restar(operando1, operando2));
+	}
+
+	public static VectorFila productoEscalar(VectorFila operando, double escalar) {
+		return new VectorFila(MatrizMath.productoEscalar(operando, escalar));
 	}
 
 	@Override
-   public double normaUno() {
-	   double norma = 0;
-		for(int i = 0; i < cantidadColumnas; i++) {
-			norma += Math.abs(coeficientes[0][i]);
-	   }
-	   return norma;
-   }
-	
-	@Override
-   public double normaDos() {
-	   double norma = 0;
-		for(int i = 0; i < cantidadColumnas; i++) {
-			norma += Math.pow(coeficientes[0][i], 2);
-	   }
-	   return Math.sqrt(norma);
-   }
-	
-	@Override
-   public double normaInfinito() {
+	public double normaUno() {
 		double norma = 0;
-		for(int i = 0; i < cantidadColumnas; i++) {
+		for (int i = 0; i < cantidadColumnas; i++) {
+			norma += Math.abs(coeficientes[0][i]);
+		}
+		return norma;
+	}
+
+	@Override
+	public double normaDos() {
+		double norma = 0;
+		for (int i = 0; i < cantidadColumnas; i++) {
+			norma += Math.pow(coeficientes[0][i], 2);
+		}
+		return Math.sqrt(norma);
+	}
+
+	@Override
+	public double normaInfinito() {
+		double norma = 0;
+		for (int i = 0; i < cantidadColumnas; i++) {
 			if (coeficientes[0][i] > norma) {
 				norma = Math.abs(coeficientes[0][i]);
 			}
-		}		
-	   return norma;
-   }
+		}
+		return norma;
+	}
+
+	// MAIN PARA HACER TEST
+
+	// MAIN PARA EL TEST
+	public static void main(String[] args) {
+		VectorFila vec1, vec2, aux;
+		vec1 = new VectorFila(2);
+		vec2 = new VectorFila(2);
+
+		vec1.setValorEn(0, 1);
+		vec1.setValorEn(1, 1);
+
+		vec2.setValorEn(0, 2);
+		vec2.setValorEn(1, 2);
+
+		for (int i = 0; i < vec1.getCantidadComponentes(); i++) {
+			System.out.println(vec1.getValorEn(i));
+		}
+
+		for (int i = 0; i < vec2.getCantidadComponentes(); i++) {
+			System.out.println(vec2.getValorEn(i));
+		}
+
+		System.out.println("Suma");
+		aux = new VectorFila(sumar(vec1, vec2));
+
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+
+		System.out.println("Resta");
+		aux = restar(vec1, vec2);
+
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+
+		System.out.println("Producto escalar x7");
+		aux = productoEscalar(vec1, 7);
+		for (int i = 0; i < aux.getCantidadComponentes(); i++) {
+			System.out.println(aux.getValorEn(i));
+		}
+	}
 }

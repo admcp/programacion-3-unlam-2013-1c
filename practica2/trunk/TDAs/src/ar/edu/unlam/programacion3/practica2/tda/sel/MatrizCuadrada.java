4c6d71deb77a3e43dca3cf419e2786786efacbf3
brunosendras@gmail.com <brunosendras@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-14 05:55:39 +0000

    Se agrego el metodo para el calculo del determinante en matriz cuadrada.

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/MatrizCuadrada.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/MatrizCuadrada.java
@@ -35,7 +35,44 @@ public class MatrizCuadrada extends MatrizMath {
 	public static MatrizCuadrada producto(MatrizCuadrada operando1, MatrizCuadrada operando2){
 		return new MatrizCuadrada(MatrizMath.producto(operando1, operando2));
 	}
+	public static double determinante(MatrizCuadrada operando) {
+		validarReferencia(operando);
+		validarDimension(operando.cantidadFilas, operando.cantidadColumnas);
+		
+		double determinante = 0;
+		int signo = 1;
+		if (operando.cantidadFilas > 2){
+			for (int i = 0; i < operando.cantidadColumnas; i++) {  
+				determinante += signo * operando.coeficientes[0][i] * determinante(obtenerSubMatriz(operando,0,i));
+				signo = signo * (-1);
+			}
+			return determinante;
+		}
+		
+		return (operando.coeficientes[0][0] * operando.coeficientes[1][1]) - (operando.coeficientes[1][0] * operando.coeficientes[0][1]);
+	}
 	
+	public static MatrizCuadrada obtenerSubMatriz(MatrizCuadrada operando, int ignorarFila, int ignorarColumna) {
+		MatrizCuadrada subMatriz = new MatrizCuadrada(operando.cantidadFilas -1);
+		int fila;
+		int columna;
+		
+		fila = 0;
+		for (int i = 0; i < operando.cantidadFilas; i++) {
+			if (i != ignorarFila) {
+				columna = 0;
+				for (int j = 0; j < operando.cantidadColumnas; j++) {
+					if (j != ignorarColumna) {
+						subMatriz.coeficientes[fila][columna] = operando.coeficientes[i][j];
+						columna++;
+					}
+				}
+				fila++;
+			}
+		}
+		return subMatriz;
+	}
+
 	public static void main(String[] args) {
 		MatrizCuadrada matriz1 = new MatrizCuadrada(2);
 		MatrizCuadrada matriz2 = new MatrizCuadrada(2);

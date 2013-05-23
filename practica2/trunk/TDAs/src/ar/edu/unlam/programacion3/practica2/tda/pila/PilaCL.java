b03e570f3ea1b822137d97f562fa7fcfe6cc1967
brunosendras@gmail.com <brunosendras@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-23 23:20:15 +0000

    Se agregó la clase PilaCL y su clase Test.

---
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/pila/PilaCL.java
@@ -0,0 +1,54 @@
+package ar.edu.unlam.programacion3.practica2.tda.pila;
+
+import ar.edu.unlam.programacion3.practica2.tda.lista.ListaEnlazada;
+
+public class PilaCL<T> implements Pila<T>{
+	
+	private ListaEnlazada<T> pila;
+
+	// CONTRUCTORES
+	public PilaCL() {
+		pila = new ListaEnlazada<T>();
+	}
+
+	// METODOS
+	@Override
+   public boolean isEmpty() {	
+	   return pila.isEmpty();
+   }
+
+	@Override
+   public void push(T elemento) {
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+		pila.addFirst(elemento);
+   }
+
+	@Override
+   public T pop() {
+		if (pila.isEmpty()) {
+			return null;
+		}
+		
+		T elemento = pila.removeFirst();
+		
+	   return elemento; 
+   }
+
+	@Override
+   public T peek() {
+		if (pila.isEmpty()) {
+			return null;
+		}
+		
+		return pila.getFirst();		
+	}
+
+	@Override
+   public void clear() {
+		while (!pila.isEmpty()) {
+			pila.removeFirst();
+		}	   
+   }
+}

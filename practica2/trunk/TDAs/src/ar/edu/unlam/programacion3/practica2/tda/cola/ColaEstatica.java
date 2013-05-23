5e8d543d6f632f6bdd77d6ef9e9c874537f39be7
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-23 11:58:39 +0000

    Ver Issue 13: Refactorizando el uso de excepciones.

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/cola/ColaEstatica.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/cola/ColaEstatica.java
@@ -1,27 +1,24 @@
 package ar.edu.unlam.programacion3.practica2.tda.cola;
 
-import java.nio.BufferOverflowException;
-import java.nio.BufferUnderflowException;
-
 public class ColaEstatica<T> implements Cola<T> {
 
 	// MIEMBROS PRIVADOS
 	private int dimension;
-	private T cola [];
+	private T cola[];
 	private int primero;
 	private int ultimo;
-	
+
 	@SuppressWarnings("unchecked")
-   public ColaEstatica(int dimension) {
-	   if (dimension == 0) {
-	   	throw new IllegalArgumentException();
-	   }
-	   
+	public ColaEstatica(int dimension) {
+		if (dimension <= 0) {
+			throw new IllegalArgumentException();
+		}
+
 		this.dimension = dimension;
-	   cola = (T[]) new Object[dimension];
-	   primero = 0;
-	   ultimo = -1;
-   }
+		cola = (T[]) new Object[dimension];
+		primero = 0;
+		ultimo = -1;
+	}
 
 	@Override
 	public boolean isEmpty() {
@@ -30,11 +27,14 @@ public class ColaEstatica<T> implements Cola<T> {
 
 	@Override
 	public void offer(T elemento) {
-		if (primero == 0 && ultimo == (dimension - 1) || 
-			 ultimo != -1 && (ultimo + 1) == primero) {
-			throw new BufferOverflowException();
+		if(elemento == null) {
+			throw new NullPointerException();
 		}
 		
+		if (primero == 0 && ultimo == (dimension - 1) || ultimo != -1 && (ultimo + 1) == primero) {
+			throw new IllegalStateException();
+		}
+
 		ultimo = (ultimo + 1) % dimension;
 		cola[ultimo] = elemento;
 	}
@@ -42,26 +42,26 @@ public class ColaEstatica<T> implements Cola<T> {
 	@Override
 	public T poll() {
 		if ((primero == 0) && (ultimo == -1)) {
-			throw new BufferUnderflowException();
+			return null;
 		}
-		
+
 		T elemento = cola[primero];
 		cola[primero] = null;
-		
+
 		if (primero == ultimo) {
 			primero = 0;
 			ultimo = -1;
 		} else {
 			primero = (primero + 1) % dimension;
 		}
-		
+
 		return elemento;
 	}
 
 	@Override
 	public T peek() {
 		if ((primero == 0) && (ultimo == -1)) {
-			throw new BufferUnderflowException();
+			return null;
 		}
 
 		return cola[primero];
@@ -71,13 +71,13 @@ public class ColaEstatica<T> implements Cola<T> {
 	public void clear() {
 		while ((primero != 0) && (ultimo != -1)) {
 			cola[primero] = null;
-			
+
 			if (primero == ultimo) {
 				primero = 0;
 				ultimo = -1;
 			} else {
 				primero = (primero + 1) % dimension;
 			}
-		}		
+		}
 	}
 }
--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/pila/PilaDinamica.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/pila/PilaDinamica.java
@@ -1,17 +1,15 @@
 package ar.edu.unlam.programacion3.practica2.tda.pila;
 
-import java.nio.BufferUnderflowException;
-
 public class PilaDinamica<T> implements Pila<T> {
-	
+
 	// MIEMBROS PRIVADOS
 	private Nodo<T> tope;
-	
+
 	// CONSTRUCTORES
 	public PilaDinamica() {
 		tope = null;
 	}
-	
+
 	// METODOS
 	@Override
 	public boolean isEmpty() {
@@ -21,36 +19,36 @@ public class PilaDinamica<T> implements Pila<T> {
 	@Override
 	public void push(T elemento) {
 		if (elemento == null) {
-			throw new IllegalArgumentException();
+			throw new NullPointerException();
 		}
-		
+
 		Nodo<T> nodo = new Nodo<T>(null, elemento, null);
-		
+
 		if (tope != null) {
 			nodo.anterior = tope;
 		}
-		
+
 		tope = nodo;
 	}
 
 	@Override
 	public T pop() {
 		if (tope == null) {
-			throw new BufferUnderflowException();
+			return null;
 		}
-		
+
 		T elemento = tope.elemento;
-		tope = tope.anterior;		
-		
+		tope = tope.anterior;
+
 		return elemento;
 	}
 
 	@Override
 	public T peek() {
-		if	(tope == null) {
-			throw new BufferUnderflowException();
+		if (tope == null) {
+			return null;
 		}
-		
+
 		return tope.elemento;
 	}
 
@@ -60,23 +58,23 @@ public class PilaDinamica<T> implements Pila<T> {
 			tope = null;
 		}
 	}
-	
-	//------------------------------
+
+	// ------------------------------
 	// Clase interna para los nodos
-	//------------------------------
-	
+	// ------------------------------
+
 	private static class Nodo<T> {
 
 		Nodo<T> anterior;
 		T elemento;
 		Nodo<T> siguiente;
-		
+
 		Nodo(Nodo<T> anterior, T elemento, Nodo<T> siguiente) {
 			this.anterior = anterior;
 			this.elemento = elemento;
 			this.siguiente = siguiente;
 		}
-		
+
 	}
 
 }

4d5bc6df7c9ba17b8581e65bfcbdef52bf92868c
anlucki <anlucki@gmail.com>
2013-05-24 19:20:10 +0000

    Terminada la implementación de ListaArray y creado su test.

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/lista/ListaArray.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/lista/ListaArray.java
@@ -1,34 +1,31 @@
 package ar.edu.unlam.programacion3.practica2.tda.lista;
 
+
 public class ListaArray<T> implements Lista<T> {
 
-	private int size;	
-	private int primero;
-	private int ultimo;
+	private int size;
 	private T[] arrayElementos;
-	
+
 	@SuppressWarnings("unchecked")
 	public ListaArray(int capacidadInicial) {
 		if (capacidadInicial <= 0) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		size = 0;
-		primero = -1;
-		ultimo = -1;
 		arrayElementos = (T[]) new Object[capacidadInicial];
 	}
-	
+
 	public ListaArray() {
 		this(10);
 	}
-	
-	//----------------------------------------------------
+
+	// ----------------------------------------------------
 	// Comportamiento derivado del contrato con <<Lista>>
-	//----------------------------------------------------
-	
+	// ----------------------------------------------------
+
 	@Override
-	public int size() {		
+	public int size() {
 		return size;
 	}
 
@@ -36,13 +33,13 @@ public class ListaArray<T> implements Lista<T> {
 	public boolean isEmpty() {
 		return size == 0;
 	}
-	
+
 	@Override
 	public boolean add(T elemento) {
-		if	(elemento == null) {
+		if (elemento == null) {
 			throw new NullPointerException();
 		}
-		
+
 		arrayElementos[size++] = elemento;
 		return true;
 	}
@@ -52,52 +49,59 @@ public class ListaArray<T> implements Lista<T> {
 		if (elemento == null) {
 			throw new NullPointerException();
 		}
+		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
 
 		for (int index = 0; index < size; index++) {
-         if (elemento.equals(arrayElementos[index])) {
-             borradoRapido(index);
-             return true;
-         }
+			if (elemento.equals(arrayElementos[index])) {
+				borradoRapido(index);
+				return true;
+			}
 		}
+		
 		return false;
 	}
 
 	private void borradoRapido(int index) {
-      int cantidadMovidos = size - index - 1;      
-      
-      if (cantidadMovidos > 0) {
-          System.arraycopy(arrayElementos, index+1, arrayElementos, index, cantidadMovidos);
-      }
-      
-      arrayElementos[--size] = null;
-   }
+		int cantidadMovidos = size - index - 1;
+
+		if (cantidadMovidos > 0) {
+			System.arraycopy(arrayElementos, index + 1, arrayElementos, index, cantidadMovidos);
+		}
+
+		arrayElementos[--size] = null;
+	}
 
 	@Override
 	public void reverse() {
-		// TODO ListaArray: void reverse()
-      for (int i = 0; i < size; i++) {
-         arrayElementos[i] = null;
-      }
-      
-     size = 0;		
+		for(int i = 0, j = size - 1; i < j; i++, j--) {
+			T aux = arrayElementos[i];
+			arrayElementos[i] = arrayElementos[j];
+			arrayElementos[j] = aux;
+		}
 	}
 
 	@Override
 	public void clear() {
-		// TODO ListaArray: void clear()
 		for (int i = 0; i < size; i++) {
-         arrayElementos[i] = null;
-      }
-      
-     size = 0;		
+			arrayElementos[i] = null;
+		}
+
+		size = 0;
 	}
 
 	@Override
 	public T get(int index) {
-      if (index < 0 || index >= size) {
-         throw new IndexOutOfBoundsException();
-      }
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
 		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
+
 		return arrayElementos[index];
 	}
 
@@ -106,38 +110,99 @@ public class ListaArray<T> implements Lista<T> {
 		if (elemento == null) {
 			throw new NullPointerException();
 		}
-		
-      if (index < 0 || index >= size) {
-         throw new IndexOutOfBoundsException();
-      }
 
-		T AntiguoElemento = arrayElementos[index];
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
+
+		T antiguoElemento = arrayElementos[index];
 		arrayElementos[index] = elemento;
-		return AntiguoElemento;
+		
+		return antiguoElemento;
 	}
 
 	@Override
 	public void add(int index, T elemento) {
-		// TODO ListaArray: void add(int index, T elemento)
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
 		
+		if(size + 1 == arrayElementos.length) { // Lista llena
+			throw new IllegalStateException();
+		}
+		
+		System.arraycopy(arrayElementos, index, arrayElementos, index + 1, size);
+		arrayElementos[index] = elemento;
+		size++;
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO ListaArray: T remove(int index)
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
+		
+		T elemento = arrayElementos[index];
+		
+		for(int i = index; i < size - 1; i++) {
+			arrayElementos[i] = arrayElementos[i + 1];
+		}
+		
+		arrayElementos[size - 1] = null;
+		
+		size--;
+		
+		return elemento;
 	}
 
 	@Override
 	public int indexOf(T elemento) {
-		// TODO ListaArray: int indexOf(Object elemento)
-		return 0;
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
+		
+		for(int i = 0; i < size; i++) {
+			if(arrayElementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 
 	@Override
 	public int lastIndexOf(T elemento) {
-		// TODO ListaArray: int lastIndexOf(Object elemento)
-		return 0;
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+		
+		if(size == 0) { // Lista vacía
+			throw new IllegalStateException();
+		}
+		
+		for(int i = size; i > 0; i--) {
+			if(arrayElementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
-	
+
 }

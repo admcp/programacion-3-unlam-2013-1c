bc0474f5410a7526d4be2438708543dbd2219e88
brunosendras@gmail.com <brunosendras@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-24 04:30:06 +0000

    Metodos implementados...
        public int size();
        public boolean isEmpty();
        public boolean add(T elemento);
        public boolean remove(T elemento);   
        public void clear();    
        public T get(int index);
        public T set(int index, T elemento);

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/lista/ListaArray.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/lista/ListaArray.java
@@ -2,15 +2,25 @@ package ar.edu.unlam.programacion3.practica2.tda.lista;
 
 public class ListaArray<T> implements Lista<T> {
 
-	private int size;
+	private int size;	
+	private int primero;
+	private int ultimo;
 	private T[] arrayElementos;
 	
+	@SuppressWarnings("unchecked")
 	public ListaArray(int capacidadInicial) {
-		// TODO ListaArray: constructor con capacidad inicial
+		if (capacidadInicial <= 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		size = 0;
+		primero = -1;
+		ultimo = -1;
+		arrayElementos = (T[]) new Object[capacidadInicial];
 	}
 	
 	public ListaArray() {
-		// TODO ListaArray: constructor
+		this(10);
 	}
 	
 	//----------------------------------------------------
@@ -18,51 +28,92 @@ public class ListaArray<T> implements Lista<T> {
 	//----------------------------------------------------
 	
 	@Override
-	public int size() {
-		// TODO ListaArray: int size()
-		return 0;
+	public int size() {		
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO ListaArray: boolean isEmpty()
-		return false;
+		return size == 0;
 	}
-
+	
 	@Override
 	public boolean add(T elemento) {
-		// TODO ListaArray: boolean add(T elemento)
-		return false;
+		if	(elemento == null) {
+			throw new NullPointerException();
+		}
+		
+		arrayElementos[size++] = elemento;
+		return true;
 	}
 
 	@Override
 	public boolean remove(T elemento) {
-		// TODO ListaArray: boolean remove(Object elemento)
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+
+		for (int index = 0; index < size; index++) {
+         if (elemento.equals(arrayElementos[index])) {
+             borradoRapido(index);
+             return true;
+         }
+		}
 		return false;
 	}
+
+	private void borradoRapido(int index) {
+      int cantidadMovidos = size - index - 1;      
+      
+      if (cantidadMovidos > 0) {
+          System.arraycopy(arrayElementos, index+1, arrayElementos, index, cantidadMovidos);
+      }
+      
+      arrayElementos[--size] = null;
+   }
 
 	@Override
 	public void reverse() {
 		// TODO ListaArray: void reverse()
-		
+      for (int i = 0; i < size; i++) {
+         arrayElementos[i] = null;
+      }
+      
+     size = 0;		
 	}
 
 	@Override
 	public void clear() {
 		// TODO ListaArray: void clear()
-		
+		for (int i = 0; i < size; i++) {
+         arrayElementos[i] = null;
+      }
+      
+     size = 0;		
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO ListaArray: T get(int index)
-		return null;
+      if (index < 0 || index >= size) {
+         throw new IndexOutOfBoundsException();
+      }
+		
+		return arrayElementos[index];
 	}
 
 	@Override
 	public T set(int index, T elemento) {
-		// TODO ListaArray: T set(int index, T elemento)
-		return null;
+		if (elemento == null) {
+			throw new NullPointerException();
+		}
+		
+      if (index < 0 || index >= size) {
+         throw new IndexOutOfBoundsException();
+      }
+
+		T AntiguoElemento = arrayElementos[index];
+		arrayElementos[index] = elemento;
+		return AntiguoElemento;
 	}
 
 	@Override

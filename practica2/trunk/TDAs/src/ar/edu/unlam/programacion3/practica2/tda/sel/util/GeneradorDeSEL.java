3263d2702c8a914b782db7e9871e4fe2b9c0e3d8
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-05-28 15:12:54 +0000

    Creado programa probador. Creado método estático en SistemaLinealDeEcuaciones para resolver desde una archivo hacia otro. Actualizado lote de pruebas en salidas esperadas para que muestre la salida correcta ante los casos que no hay soluciones posibles y la documentación correspondiente. Agregadas salidas generadas al repositorio.

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/util/GeneradorDeSEL.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/util/GeneradorDeSEL.java
@@ -10,16 +10,16 @@ import java.util.Random;
 public class GeneradorDeSEL {
 	private static final String NEWLINE = System.getProperty("line.separator");
 	private final static String outPath = "doc/loteDePruebas/entradas/";
-	
+
 	public static void main(String[] args) {
 		// Variables para escritura
 		File file = null;
 		PrintWriter printWriter = null;
-		
+
 		// Variables para lectura desde consola
 		InputStreamReader inputStreamReader = null;
 		BufferedReader inputBufferedReader = null;
-		
+
 		// Valores aleatorios
 		Random random = new Random(System.nanoTime());
 
@@ -27,77 +27,82 @@ public class GeneradorDeSEL {
 			// Abrimos la consola
 			inputStreamReader = new InputStreamReader(System.in);
 			inputBufferedReader = new BufferedReader(inputStreamReader);
-			
+
 			// Leemos el nombre del archivo
-			System.out.print("Nombre del archivo de salida (sin extension): ");
+			System.out.print("Nombre del archivo de salida (sin extensi贸n): ");
 			String fileName = inputBufferedReader.readLine();
-			
+
 			// Leemos la dimension del sistema
 			int dimension = 0;
-			
-			System.out.print("Dimensi髇 del sistema aleatoria (s/n) (2 a 1000): ");
-			switch(inputBufferedReader.readLine()) {
-				case "s": case "S": 
-					break;
-				case "n": case "N":
-					System.out.print("Dimensi髇 del sistema (2 a 1000): ");
-					dimension = Integer.parseInt(inputBufferedReader.readLine());
-					break;
-				default:
-					throw new Error("Valor erroneo");
+
+			System.out.print("Dimensi贸n del sistema aleatoria (s/n) (2 a 1000): ");
+			switch (inputBufferedReader.readLine()) {
+			case "s":
+			case "S":
+				break;
+			case "n":
+			case "N":
+				System.out.print("Dimensi贸n del sistema (2 a 1000): ");
+				dimension = Integer.parseInt(inputBufferedReader.readLine());
+				break;
+			default:
+				throw new Error("Valor err贸neo");
 			}
 
 			// Leemos los coeficientes (aleatorio, m谩ximos y m铆nimos)
 			double valorMin = -1;
 			double valorMax = -1;
-			
-			System.out.print("Valor Minimo: ");
+
+			System.out.print("Valor M铆nimo: ");
 			valorMin = Double.parseDouble(inputBufferedReader.readLine());
-			System.out.print("Valor Maximo (" + (valorMin + 1)+ " a ...): ");
+			System.out.print("Valor M谩ximo (" + (valorMin + 1) + " a ...): ");
 			valorMax = Double.parseDouble(inputBufferedReader.readLine());
-			
+
 			boolean valoresSucesivos = false;
-			System.out.print("valores aleatorios o sucesivos (a/s): ");
-			switch(inputBufferedReader.readLine()) {
-				case "a": case "A": 
-					break;
-				case "s": case "S":
-					valoresSucesivos = true;
-					break;
-				default:
-					throw new Error("Valor erroneo");
+			System.out.print("Valores aleatorios o sucesivos (a/s): ");
+			switch (inputBufferedReader.readLine()) {
+			case "a":
+			case "A":
+				break;
+			case "s":
+			case "S":
+				valoresSucesivos = true;
+				break;
+			default:
+				throw new Error("Valor err贸neo");
 			}
 
 			boolean truncar = true;
 			System.out.print("Truncar valores (s/n): ");
-			switch(inputBufferedReader.readLine()) {
-				case "s": case "S": 
-					break;
-				case "n": case "N":
-					truncar = false;
-					break;
-				default:
-					throw new Error("Valor erroneo");
+			switch (inputBufferedReader.readLine()) {
+			case "s":
+			case "S":
+				break;
+			case "n":
+			case "N":
+				truncar = false;
+				break;
+			default:
+				throw new Error("Valor err贸neo");
 			}
-			
+
 			// Abrimos el archivo de salida
 			file = new File(outPath + fileName + ".in");
 			printWriter = new PrintWriter(file);
 
 			// Primera linea indica la dimension del sistema
-			if(dimension <= 0) {
+			if (dimension <= 0) {
 				dimension = random.nextInt(99) + 2;
 			}
 			printWriter.println(dimension);
-			
 
 			double valor = 0;
-			String format = truncar ? "%d %d %.0f" + NEWLINE  : "%d %d %.2f" + NEWLINE;
+			String format = truncar ? "%d %d %.0f" + NEWLINE : "%d %d %.2f" + NEWLINE;
 
-			for(int i = 0; i < dimension; i++) {
-				for(int j = 0; j < dimension; j++) {
-					if(!valoresSucesivos) {
-						printWriter.format(format, i, j,random.nextDouble() * (valorMax - valorMin) + valorMin);
+			for (int i = 0; i < dimension; i++) {
+				for (int j = 0; j < dimension; j++) {
+					if (!valoresSucesivos) {
+						printWriter.format(format, i, j, random.nextDouble() * (valorMax - valorMin) + valorMin);
 					} else {
 						printWriter.format(format, i, j, valor);
 						valor += (valorMax - valorMin) / dimension;
@@ -107,31 +112,30 @@ public class GeneradorDeSEL {
 			}
 
 			format = truncar ? "%.0f" + NEWLINE : "%.2f" + NEWLINE;
-			for(int i = 0; i < dimension; i++) {
-				if(!valoresSucesivos) {
+			for (int i = 0; i < dimension; i++) {
+				if (!valoresSucesivos) {
 					printWriter.format(format, random.nextDouble() * (valorMax - valorMin) + valorMin);
 				} else {
 					printWriter.format(format, valor);
 					valor += (valorMax - valorMin) / dimension;
-				}				
+				}
 			}
-			
+
 		} catch (IOException e) {
-			e.printStackTrace();		
-		}
-		finally {
+			e.printStackTrace();
+		} finally {
 			try {
 				// Cerramos el stream de escritura
-				if(printWriter != null) {
+				if (printWriter != null) {
 					printWriter.close();
 				}
 				// Cerramos el stream de lectura desde consola
-				if(inputBufferedReader != null) {
+				if (inputBufferedReader != null) {
 					inputBufferedReader.close();
 				}
-			} catch(IOException ex) {
+			} catch (IOException ex) {
 				ex.printStackTrace();
-			}			
-		}		
+			}
+		}
 	}
 }
--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/util/SerializadorDeSEL.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practica2/tda/sel/util/SerializadorDeSEL.java
@@ -8,16 +8,14 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import ar.edu.unlam.programacion3.practica2.tda.sel.MatrizCuadrada;
 import ar.edu.unlam.programacion3.practica2.tda.sel.SistemaLinealDeEcuaciones;
-import ar.edu.unlam.programacion3.practica2.tda.sel.VectorColumna;
 import ar.edu.unlam.programacion3.practica2.tda.sel.exceptions.FilaDeCerosException;
 import ar.edu.unlam.programacion3.practica2.tda.sel.exceptions.MatrizSingularException;
 
 public class SerializadorDeSEL {
 
 	private final static String inPath = "doc/loteDePruebas/entradas/";
-	private final static String outPath = "doc/loteDePruebas/SalidasGeneradas/";
+	private final static String outPath = "doc/loteDePruebas/salidasGeneradas/";
 	private final static String inExtension = ".in";
 	private final static String outExtension = ".out";
 
@@ -52,71 +50,9 @@ public class SerializadorDeSEL {
 				file = new File(fullOutPath);
 				printWriter = new PrintWriter(file);
 
-				String buffer;
-				String[] splitBuffer;
-
-				/*
-				 * Estructura esperada del archivo "*.in": 
-				 * n (dimensión del sistema) 
-				 * i j <valor> (i-fila j-columna valor) x n^2 -matriz de coeficientes- 
-				 * i (i-fila) x n -vector independiente-
-				 */
-
-				// Leemos la primer línea del archivo de entrada
-				buffer = bufferedReader.readLine();
-				int dimension = Integer.parseInt(buffer);
-
-				// Creamos una matriz para contener los coeficientes
-				double[][] matriz = new double[dimension][dimension];
-
-				// Llenamos la matriz con los valores del archivo
-				int ii, jj;
-				for (int i = 0; i < dimension; i++) {
-					for (int j = 0; j < dimension; j++) {
-						buffer = bufferedReader.readLine();
-						splitBuffer = buffer.split(" ");
-						ii = Integer.parseInt(splitBuffer[0]);
-						jj = Integer.parseInt(splitBuffer[1]);
-						if (ii != i || jj != j) {
-							throw new Error("Archivo mal formado");
-						}
-						matriz[i][j] = Double.parseDouble(splitBuffer[2]);
-					}
-				}
-				
-				// Creamos un vector para contener los valores del vector solución
-				double[] vector = new double[dimension];
-
-				// Llenamos el vector con los valores del archivo
-				for (int i = 0; i < dimension; i++) {
-					buffer = bufferedReader.readLine();
-					vector[i] = Double.parseDouble(buffer);
-				}
-
-				// Deserializamos ambas estructuras
-				SistemaLinealDeEcuaciones sistemaLineal =
-						new SistemaLinealDeEcuaciones(new MatrizCuadrada(matriz), new VectorColumna(vector));
-				
-				// Resolver sistema
-				VectorColumna vectorSolucion = sistemaLineal.resolver();
-				
-				// Calcular error de solución
-				double error = sistemaLineal.getError();
-				
-				/*
-				 * Estructura esperada del archivo "*.out": 
-				 * n (dimensión del sistema) 
-				 * i (i-fila) x n -vector solución-
-				 * e (error)
-				 */
-				
-				printWriter.println(dimension);
-				for(int i = 0; i < dimension; i++) {
-					printWriter.println(vectorSolucion.getValorEn(i));
-				}
-				
-				printWriter.print(error != Double.NaN ? error : 0);
-				
+				System.out.println("Procesando " + fullInPath + "...");
+				SistemaLinealDeEcuaciones.resolverSELDesdeArchivo(bufferedReader, printWriter);
+				System.out.println("Fin de proceso de: " + fullInPath + ".");
 
 			} catch(FileNotFoundException ex) {
 				ex.printStackTrace();

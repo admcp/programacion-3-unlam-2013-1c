67f996aa2fe8a9b56c3087e478dde763686a534a
anlucki <anlucki@gmail.com>
2013-06-04 19:56:14 +0000

    - Creada una cola de prioridad máxima y otra mínima (de acuerdo al montículo). - Creados test de cola de prioridad (con una clase llamada WeirdInteger que tiene un valor y una prioridad). - Actualizados diagramas UML. - Corregido MonticuloMinimo que no redimensionaba para arriba (no agrandaba).

--- practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practicaadhoc/tda/monticulo/MonticuloMinimo.java
+++ practica2/trunk/TDAs/src/ar/edu/unlam/programacion3/practicaadhoc/tda/monticulo/MonticuloMinimo.java
@@ -1,14 +1,14 @@
 package ar.edu.unlam.programacion3.practicaadhoc.tda.monticulo;
 
 public class MonticuloMinimo<T extends Comparable<T>> implements Monticulo<T> {
-	
+
 	private T[] monticulo;
 	private int size = 0;
-	
+
 	public MonticuloMinimo() {
 		this(10);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public MonticuloMinimo(int initialCapacity) {
 		if (initialCapacity <= 0) {
@@ -16,79 +16,81 @@ public class MonticuloMinimo<T extends Comparable<T>> implements Monticulo<T> {
 		}
 		monticulo = (T[]) new Comparable[initialCapacity + 1];
 	}
-	
-	@Override
-   public boolean isEmpty() {
-	   return size == 0;
-   }
-		
-	@Override
-   public void add(T item) {
-	   if	(item == null) {
-	   	throw new NullPointerException();
-	   }
-	   
-	   monticulo[++size] = item;
-	   ordenarAdd(size);	   
-   }
 
 	@Override
-   public T remove() {
-	   if (size == 0) {
-	   	throw new IllegalStateException();
-	   }
-	   
-	   T item = monticulo[1];
-	   intercambiar(1, size);
-	   monticulo[size] = null;
-	   size--;
-	   
-	   ordenarRemove(1);
-	   
-	   if (size > 0 && size == monticulo.length / 4) {
-	   	resize(monticulo.length / 2);
-	   }
-	   
-	   return item;
-   }
+	public boolean isEmpty() {
+		return size == 0;
+	}
 
 	@Override
-   public T peek() {
-	   if (size == 0) {
-	   	throw new IllegalStateException();
-	   }
-	   
-	   return monticulo[1];
-   }
+	public void add(T item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
+		if (monticulo.length == size + 1) {
+			resize(monticulo.length * 2);
+		}
+		monticulo[++size] = item;
+		ordenarAdd(size);
+	}
+
+	@Override
+	public T remove() {
+		if (size == 0) {
+			throw new IllegalStateException();
+		}
+
+		T item = monticulo[1];
+		intercambiar(1, size);
+		monticulo[size] = null;
+		size--;
+
+		ordenarRemove(1);
+
+		if (size > 0 && size == monticulo.length / 4) {
+			resize(monticulo.length / 2);
+		}
+
+		return item;
+	}
+
+	@Override
+	public T peek() {
+		if (size == 0) {
+			throw new IllegalStateException();
+		}
+
+		return monticulo[1];
+	}
 
 	private void ordenarAdd(int indiceActual) {
 		while (indiceActual > 1 && esMayor(indicePadre(indiceActual), indiceActual)) {
 			intercambiar(indicePadre(indiceActual), indiceActual);
-			indiceActual = indicePadre(indiceActual); 
+			indiceActual = indicePadre(indiceActual);
 		}
 	}
-	
+
 	private void ordenarRemove(int indiceActual) {
-		while (indiceHijoIzquierdo(indiceActual) <= size ) {
+		while (indiceHijoIzquierdo(indiceActual) <= size) {
 			int indice = indiceHijoIzquierdo(indiceActual);
 			if (indice < size && esMayor(indice, indiceHijoDerecho(indiceActual))) {
 				indice = indiceHijoDerecho(indiceActual);
 			}
-			
-			if(esMenor(indiceActual, indice) || esIgual(indiceActual, indice)) {
+
+			if (esMenor(indiceActual, indice) || esIgual(indiceActual, indice)) {
 				// ya esta organizado del monticulo.
 				break;
 			}
-			
+
 			intercambiar(indiceActual, indice);
 			indiceActual = indice;
 		}
 	}
-	
+
 	private int indiceHijoIzquierdo(int indicePadre) {
 		return indicePadre * 2;
 	}
-	
+
 	private boolean esMayor(int indice1, int indice2) {
 		return monticulo[indice1].compareTo(monticulo[indice2]) > 0;
 	}
@@ -100,21 +102,21 @@ public class MonticuloMinimo<T extends Comparable<T>> implements Monticulo<T> {
 	private boolean esIgual(int indice1, int indice2) {
 		return monticulo[indice1].compareTo(monticulo[indice2]) == 0;
 	}
-	
-	private int indiceHijoDerecho(int indicePadre){
-		return (indicePadre * 2) +1;
+
+	private int indiceHijoDerecho(int indicePadre) {
+		return (indicePadre * 2) + 1;
 	}
-	
+
 	private int indicePadre(int indiceHijo) {
 		return (int) Math.floor(indiceHijo / 2);
 	}
-	
+
 	private void intercambiar(int indice1, int indice2) {
 		T aux = monticulo[indice1];
 		monticulo[indice1] = monticulo[indice2];
-		monticulo[indice2] = aux;				
+		monticulo[indice2] = aux;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void resize(int newSize) {
 		T[] newArray = (T[]) new Comparable[newSize];
@@ -127,12 +129,12 @@ public class MonticuloMinimo<T extends Comparable<T>> implements Monticulo<T> {
 	@Override
 	public String toString() {
 		StringBuilder arrayPrint = new StringBuilder();
-		for(int i = 1; i < size + 1; i++) {
+		for (int i = 1; i < size + 1; i++) {
 			arrayPrint.append(monticulo[i] + " ");
 		}
 		return arrayPrint.toString();
 	}
-	
+
 	public static void main(String[] args) {
 		Monticulo<Integer> monticuloMin = new MonticuloMinimo<Integer>();
 		monticuloMin.add(20);
@@ -143,7 +145,7 @@ public class MonticuloMinimo<T extends Comparable<T>> implements Monticulo<T> {
 		monticuloMin.add(8);
 		monticuloMin.add(9);
 		monticuloMin.add(5);
-		
+
 		while (!monticuloMin.isEmpty()) {
 			System.out.println(monticuloMin);
 			monticuloMin.remove();

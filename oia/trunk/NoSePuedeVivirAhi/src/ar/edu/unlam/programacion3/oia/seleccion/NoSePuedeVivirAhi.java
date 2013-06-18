be090e7f29d02eedd51fee4d45b031230d9cc047
anlucki@gmail.com <anlucki@gmail.com@dfd4c138-39d4-e291-5a5b-5730b611bd11>
2013-06-18 07:35:00 +0000

    Terminado ejercicio OIA "No Se Puede Vivir Ahi". Falta documentacion del lote de pruebas.

--- oia/trunk/NoSePuedeVivirAhi/src/ar/edu/unlam/programacion3/oia/seleccion/NoSePuedeVivirAhi.java
+++ oia/trunk/NoSePuedeVivirAhi/src/ar/edu/unlam/programacion3/oia/seleccion/NoSePuedeVivirAhi.java
@@ -5,22 +5,91 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NoSePuedeVivirAhi {
 
 	public static void main(String[] args) {
-		File archIn = null;
+		File archIn = null, archOut = null;
 		FileReader archReader = null;
 		BufferedReader bufferIn = null;
+		PrintWriter bufferOut = null;
 
 		try {
 			archIn = new File("clima.in");
 			archReader = new FileReader(archIn);
 			bufferIn = new BufferedReader(archReader);
+			archOut = new File("clima.out");
+			bufferOut = new PrintWriter(archOut);
 
+			String buffer;
+			String[] splitBuffer;
+			
 			int cantidadLugares;
 			int cantidadRegistros;
-
+			List<Lugar> listaLugares = new ArrayList<Lugar>();
+			
+			buffer = bufferIn.readLine();
+			cantidadLugares = Integer.parseInt(buffer);
+			
+			for(int i = 0; i < cantidadLugares; i++) {
+				buffer = bufferIn.readLine();
+				cantidadRegistros = Integer.parseInt(buffer);
+				
+				int tempMin = 100, tempMax = -100;
+				int auxTempMin = 0, auxTempMax = 0;
+				
+				for(int j = 0; j < cantidadRegistros; j++) {
+					buffer = bufferIn.readLine();
+					splitBuffer = buffer.split(" ");
+					
+					auxTempMin = Integer.parseInt(splitBuffer[0]);
+					auxTempMax = Integer.parseInt(splitBuffer[1]);
+					
+					if(auxTempMin <= tempMin) {
+						tempMin = auxTempMin;
+					}
+					
+					if(auxTempMax >= tempMax) {
+						tempMax = auxTempMax;
+					}
+					
+					
+				}
+				
+				listaLugares.add(new Lugar(tempMin, tempMax));
+				
+			}
+			
+			for(int i = 0; i < listaLugares.size(); i++) {
+				for(int j = 0; j < listaLugares.size(); j++) {
+					if(i != j) {
+						switch (listaLugares.get(i).compareTo(listaLugares.get(j))) {
+							case 1: // i más hostil que j
+								listaLugares.get(i).cantHostiles++;
+								break;
+							case 0: // i igualmente hostil que j 
+								listaLugares.get(i).cantHostiles++;
+								listaLugares.get(j).cantHostiles++;
+								break;
+							case -1: // i no es comparable con j
+								listaLugares.get(i).cantNoComparables++;
+								break;
+						}
+					}
+				}
+			}
+			
+			int index = 1;
+			for(Lugar lugar : listaLugares) {
+				if(lugar.cantHostiles > 0) {
+					bufferOut.println(index + " " + lugar.cantNoComparables);
+				}
+				index++;
+			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -32,47 +101,40 @@ public class NoSePuedeVivirAhi {
 				if (bufferIn != null) {
 					bufferIn.close();
 				}
+				if(bufferOut != null) {
+					bufferOut.close();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	private class Lugar {
-		int tempMin;
-		int tempMax;
-		int cantHostiles;
-		int cantNoComparables;
+}
 
-		public Lugar(int tempMin, int tempMax) {
-			this.tempMax = tempMax;
-			this.tempMin = tempMin;
-		}
+class Lugar implements Comparable<Lugar>{
 
-		public int getTempMin() {
-			return tempMin;
-		}
+	int tempMin;
+	int tempMax;
+	int cantHostiles;
+	int cantNoComparables;
 
-		public int getTempMax() {
-			return tempMax;
-		}
-
-		public int getCantHostiles() {
-			return cantHostiles;
-		}
-
-		public int getCantNoComparables() {
-			return cantNoComparables;
-		}
-
-		public void incCantHostiles() {
-			cantHostiles++;
-		}
-
-		public void incCantNoComparables() {
-			cantNoComparables++;
-		}
-
+	public Lugar(int tempMin, int tempMax) {
+		this.tempMax = tempMax;
+		this.tempMin = tempMin;
 	}
 
+	@Override
+	public int compareTo(Lugar otro) {
+		if((this.tempMin < otro.tempMin && this.tempMax > otro.tempMax) || // m_a < m_b && M_a > M_b   
+			(this.tempMin == otro.tempMin && this.tempMax > otro.tempMax) || // m_a = m_b && M_a > M_b 
+			(this.tempMin < otro.tempMin && this.tempMax == otro.tempMax)) { // m_a < m_b && M_a == M_b
+			return 1;
+		} else if((this.tempMin == otro.tempMin && this.tempMax == otro.tempMax)) { // m_a == m_b && M_a == M_b
+			return 0;
+		} else {  // no comparable
+			return -1;
+		}
+	}
+ 
 }
